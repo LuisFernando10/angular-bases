448a6c639efec9958aa1cc5dd55fde8dbc29b76a
Luis Fernando <fernando.chamorro@onetree.com>
2023-04-18 21:14:47 -0500

    Moved functionality app-counter component in it

--- src/app/Counter/counter-component.ts
+++ src/app/Counter/counter-component.ts
@@ -2,8 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola {{name}}</h1>',
+  template: `<p>Bienvenido a Angular</p>
+    <h3>El contador es: {{ counter }}</h3>
+    <button (click)="handleCounter(1)">+1</button> -
+    <button (click)="handleCounterReset()">Reset</button> -
+    <button (click)="handleCounter(-1)">-1</button>`,
 })
 export class CounterComponent {
-  name = 'Otro Componente';
+  public counter = 10;
+
+  handleCounter(value: number): void {
+    this.counter += value;
+  }
+
+  handleCounterReset(): void {
+    this.counter = 10;
+  }
+
+  constructor() {}
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public counter = 10;
   public nombre = 'Luis';
   public title: string = 'my-first-angular-project';
-
-  handleCounter(value: number): void {
-    this.counter += value;
-  }
-
-  handleCounterReset(): void {
-    this.counter = 10;
-  }
 }
