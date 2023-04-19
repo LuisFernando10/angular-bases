4e7a4353c6ed9e9bdcd93faaaa7fb4c8ae7543a3
Luis Fernando <fernando.chamorro@onetree.com>
2023-04-18 20:32:10 -0500

    Introduction to app-component and property handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  public counter = 10;
+  public nombre = 'Luis';
+  public title: string = 'my-first-angular-project';
 }
