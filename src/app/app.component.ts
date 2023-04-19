41714b35220c83b980acea6832147c9a9917375f
Luis Fernando <fernando.chamorro@onetree.com>
2023-04-18 20:53:22 -0500

    Counter functions example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,12 @@ export class AppComponent {
   public counter = 10;
   public nombre = 'Luis';
   public title: string = 'my-first-angular-project';
+
+  handleCounter(value: number): void {
+    this.counter += value;
+  }
+
+  handleCounterReset(): void {
+    this.counter = 10;
+  }
 }
