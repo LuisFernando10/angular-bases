c1e31fbd489d10b038c6169785cc974f35491be5
Luis Fernando <fernando.chamorro@onetree.com>
2023-04-20 21:42:58 -0500

    Created my first Angular (module)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule, CounterModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CounterModule, HeroesModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MainPageComponent } from './pages/main-page.component';
+
+@NgModule({
+  declarations: [MainPageComponent],
+  exports: [MainPageComponent],
+})
+export class DbzModule {}
