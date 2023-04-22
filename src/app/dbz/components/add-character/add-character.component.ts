43438a68e8add417a19d0326328e3b8597cfc5fe
Luis Fernando <fernando.chamorro@onetree.com>
2023-04-21 22:01:58 -0500

    Implemented the [(ngModel)] directive to allow the 'two way data binding'

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { ICharacter } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-add-character',
   templateUrl: './add-character.component.html',
-  styleUrls: ['./add-character.component.css'],
 })
-export class AddCharacterComponent {}
+export class AddCharacterComponent {
+  public character: ICharacter = {
+    name: '',
+    power: 0,
+  };
+
+  emitCharacter(): void {
+    console.log(this.character);
+  }
+}
